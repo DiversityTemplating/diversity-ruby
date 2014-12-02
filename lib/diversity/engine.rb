e5eea5c5d8d50a886d01aa6c46513a87c46153ee
Lars Olsson <lasso@textalk.se>
2014-12-02 14:15:56 +0100

    Handle minification

--- lib/diversity/engine.rb
+++ lib/diversity/engine.rb
@@ -12,10 +12,13 @@ module Diversity
     # Default options for engine
     DEFAULT_OPTIONS = {
       backend_url: nil, # Optional, might be overridden in render
-      minify_base_dir: File.join(Dir.tmpdir, 'diversity', 'minified'),
-      minify_css: false,
-      minify_js: false,
-      minify_remotes: false,
+      minification: { # Only used when minification is active
+        base_dir: File.join(Dir.tmpdir, 'diversity', 'minified'),
+        base_url: '/minified',
+        minify_css: false,
+        minify_js: false,
+        minify_remotes: false
+      },
       registry: nil
     }
 
@@ -28,9 +31,7 @@ module Diversity
         @options[:registry].is_a?(Registry::Base)
     end
 
-    def options
-      @options
-    end
+    attr_reader :options
 
     # Renders a component
     #
@@ -54,9 +55,7 @@ module Diversity
       # Traverse the component_settings to expand sub-components
       expanded_settings = expand_settings(schema, component_settings, context, path, component)
 
-      html = render_template(component, context, expanded_settings)
-      #debug("Rendered: #{html}\n")
-      html
+      render_template(component, context, expanded_settings)
     end
 
 
@@ -75,9 +74,9 @@ module Diversity
 
           # Get the sub_schema from schema.  This only works with simple schema, should really be
           # done with a json-schema-lib that can expand, chose one-of etc.
-          if schema.has_key?('properties') and schema['properties'].has_key?(key)
+          if schema.key?('properties') and schema['properties'].key?(key)
             sub_schema = schema['properties'][key]
-          elsif schema.has_key?('additionalProperties')
+          elsif schema.key?('additionalProperties')
             sub_schema = schema['additionalProperties']
           else
             puts "FAIL: Trying to add setting #{key} to #{last_component} at /#{path.join('/')} " +
@@ -85,10 +84,10 @@ module Diversity
             return component_settings
           end
 
-          if sub_schema.has_key?('format') and sub_schema['format'] == 'diversity'
+          if sub_schema.key?('format') && sub_schema['format'] == 'diversity'
             # Replace the setting with HTML output from the component
-            version         = sub_settings.has_key?('version' ) ? sub_settings['version' ] : nil
-            subsub_settings = sub_settings.has_key?('settings') ? sub_settings['settings'] : nil
+            version         = sub_settings.key?('version' ) ? sub_settings['version'] : nil
+            subsub_settings = sub_settings.key?('settings') ? sub_settings['settings'] : nil
             sub_component   = get_component(sub_settings['component'], version)
 
             expanded_settings[key] =
@@ -101,7 +100,7 @@ module Diversity
       elsif component_settings.is_a?(Array)
         expanded_settings = []
 
-        # Pick schema for array items from schema items.  …this is also simplified…
+        # Pick schema for array items from schema items.  ...this is also simplified...
         sub_schema = schema['items']
 
         # @todo Check additionalItems
@@ -110,7 +109,7 @@ module Diversity
           sub_path = path.clone
           sub_path  << index
 
-          if sub_schema.has_key?('format') and sub_schema['format'] == 'diversity'
+          if sub_schema.key?('format') && sub_schema['format'] == 'diversity'
             # Ignore bad settings; they are warned about in schema validation.
             next unless sub_settings.is_a?(Hash)
 
@@ -121,7 +120,6 @@ module Diversity
 
             expanded_settings <<
               { componentHTML: render(sub_component, context, subsub_settings, sub_path) }
-            #debug("Rendered: #{expanded_settings.last}")
           else
             expanded_settings << expand_settings(sub_schema, sub_settings, context, sub_path)
           end
@@ -177,30 +175,26 @@ module Diversity
       # Add angularBootstrap, scripts and styles for this level.
       mustache_settings['angularBootstrap'] =
         "angular.bootstrap(document,#{settings.angular.to_json});"
-      if @options[:minify_js]
-        mustache_settings['scripts'] = settings.minified_scripts(
-                                         @options[:minify_base_dir],
-                                         context[:theme_id],
-                                         context[:theme_timestamp],
-                                         @options[:minify_remotes]
-                                       )
+
+      # Should we use minification?
+      if @options[:minification][:minify_js] || @options[:minification][:minify_js]
+        minify_options = @options[:minification].dup
+        minify_options[:filename] = context[:minify_filename]
+      end
+      if @options[:minification][:minify_js]
+        mustache_settings['scripts'] = settings.minified_scripts(minify_options)
       else
         mustache_settings['scripts'] = settings.scripts
       end
-      if @options[:minify_css]
-        mustache_settings['styles'] = settings.minified_styles(
-                                        @options[:minify_base_dir],
-                                        context[:theme_id],
-                                        context[:theme_timestamp],
-                                        @options[:minify_remotes]
-                                      )
+      if @options[:minification][:minify_css]
+        mustache_settings['styles'] = settings.minified_styles(minify_options)
       else
-        mustache_settings['styles' ] = settings.styles
+        mustache_settings['styles'] = settings.styles
       end
       begin
         mustache_settings[:l10n] = settings.l10n(context[:language])
       rescue Encoding::UndefinedConversionError => e
-        fail Diversity::Exception, "Bad json in l10n of #{component}: #{e}\n" +
+        raise Diversity::Exception, "Bad json in l10n of #{component}: #{e}\n" +
           "We have collected: #{settings.l10n(context[:language]).inspect}\n" +
           "With system encoding: #{Encoding.default_external}"
       end
--- lib/diversity/engine/settings.rb
+++ lib/diversity/engine/settings.rb
@@ -4,6 +4,14 @@ module Diversity
     # A simple wrapper for the context used when rendering
     class Settings
       include Common
+
+      DEFAULT_MINIFY_OPTIONS = {
+        base_dir: nil,
+        base_url: nil,
+        filename: nil,
+        minify_remotes: false
+      }
+
       def initialize(registry)
         @component_set = Diversity::ComponentSet.new(registry)
       end
@@ -42,41 +50,47 @@ module Diversity
         end
       end
 
-      def minified_scripts(base_dir, theme_id, theme_timestamp, minify_remotes = false)
+      def minified_scripts(options = {})
+        opts = DEFAULT_MINIFY_OPTIONS.merge(options)
+        fail 'Must have a base dir' unless opts[:base_dir]
+        fail 'Must have a base url' unless opts[:base_url]
+        opts[:filename] = random_name unless opts[:filename]
         scripts = []
-        path = File.expand_path(File.join(base_dir, 'scripts', "#{theme_id}-#{theme_timestamp.to_i}"))
+        path = File.expand_path(File.join(opts[:base_dir], 'scripts', opts[:filename]))
         minified_exist = File.exist?(path)
         require 'uglifier'
         uglifier = Uglifier.new
         minified = ''
         @component_set.to_a.each do |component|
           component.scripts.each do |script|
-            if !remote?(script) || minify_remotes
+            if !remote?(script) || opts[:minify_remotes]
               next if minified_exist
               data = safe_load(script)
-              minified << uglifier.compile(data) if data
+              minified << uglifier.compile(data << "\n") if data
             else
               scripts << script
             end
           end
         end
-        unless minified_exist || minified.empty?
-          create_minified_file(path, minified)
-          scripts.unshift(path)
-        end
+        create_minified_file(path, minified) unless minified_exist || minified.empty?
+        scripts.unshift(minified_url(opts[:base_url], path)) if minified_exist || !minified.empty?
         scripts
       end
 
-      def minified_styles(base_dir, theme_id, theme_timestamp, minify_remotes = false)
+      def minified_styles(options = {})
+        opts = DEFAULT_MINIFY_OPTIONS.merge(options)
+        fail 'Must have a base dir' unless opts[:base_dir]
+        fail 'Must have a base url' unless opts[:base_url]
+        opts[:filename] = random_name unless opts[:filename]
         styles = []
-        path = File.expand_path(File.join(base_dir, 'styles', "#{theme_id}-#{theme_timestamp.to_i}"))
+        path = File.expand_path(File.join(opts[:base_dir], 'styles', opts[:filename]))
         minified_exist = File.exist?(path)
         require 'cssminify'
         compressor = CSSminify.new
         minified = ''
         @component_set.to_a.each do |component|
           component.styles.each do |style|
-            if !remote?(style) || minify_remotes
+            if !remote?(style) || opts[:minify_remotes]
               next if minified_exist
               data = safe_load(style)
               minified << compressor.compress(data << "\n") if data
@@ -85,10 +99,8 @@ module Diversity
             end
           end
         end
-        unless minified_exist || minified.empty?
-          create_minified_file(path, minified)
-        end
-        styles.unshift(minified_url(path))
+        create_minified_file(path, minified) unless minified_exist || minified.empty?
+        styles.unshift(minified_url(opts[:base_url], path)) if minified_exist || !minified.empty?
         styles
       end
 
@@ -102,16 +114,39 @@ module Diversity
 
       private
 
+      # Writes minified data to file system.
+      #
+      # @param [String] path
+      # @param [String] data
+      # @return [String]
       def create_minified_file(path, data)
         require 'fileutils'
+        path = File.expand_path(path)
         FileUtils.mkdir_p(File.dirname(path), mode: 0775)
         File.open(path, 'w') { |file| file.write(data) }
         path
       end
 
-      def minified_url(path)
-        parts = path.split(File::SEPARATOR).map { |p| p.empty? ? File::SEPARATOR : p }
-        '/minified' << '/' << parts[-2] << '/' << parts[-1]
+      # Returns an URL that can be used to access the specified minified content
+      #
+      # @param [String] base_url
+      # @param [String] path
+      # @return [String]
+      def minified_url(base_url, path)
+        url = base_url.dup
+        url << '/' unless url[-1] == '/' # Adds trailing slash to base url if needed
+        pt = File.expand_path(path)
+        parts = pt.split(File::SEPARATOR).map { |p| p.empty? ? File::SEPARATOR : p }
+        url << parts[-2] << '/' << parts[-1]
+        url
+      end
+
+      # Generates a random filename
+      #
+      # @return [String]
+      def random_name
+        require 'securerandom'
+        SecureRandom.urlsafe_base64
       end
     end
   end
